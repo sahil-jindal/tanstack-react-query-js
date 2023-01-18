4d45c3d7b42508ada530e4f573c07471435e9839
Sahil Jindal <sahiljindal.thebest@gmail.com>
2023-01-18 07:15:14 +0530

    Added GET /posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,46 @@
+import { useQuery, useMutation } from "@tanstack/react-query";
+
+const POSTS = [
+    { id: 1, title: "Post 1" },
+    { id: 2, title: "Post 2" },
+];
+
+function wait(duration) {
+    return new Promise((resolve) => setTimeout(resolve, duration));
+}
+
 function App() {
-    return <h1>TanStack Query</h1>;
+    const postsQuery = useQuery({
+        queryKey: ["posts"],
+        queryFn: () => wait(1000).then(() => [...POSTS]),
+    });
+
+    const newPostMutation = useMutation({
+        mutationFn: (title) => {
+            return wait(1000).then(() =>
+                POSTS.push({ id: crypto.randomUUID(), title })
+            );
+        },
+    });
+
+    if (postsQuery.isLoading) {
+        return <h1>Loading...</h1>;
+    }
+
+    if (postsQuery.isError) {
+        return <pre>{JSON.stringify(postsQuery.error)}</pre>;
+    }
+
+    return (
+        <div>
+            {postsQuery.data.map((post) => (
+                <div key={post.id}>{post.title}</div>
+            ))}
+            <button onClick={() => newPostMutation.mutate("New Post")}>
+                Add Now
+            </button>
+        </div>
+    );
 }
 
 export default App;
